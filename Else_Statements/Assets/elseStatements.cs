b7667853cc4034a47441aba126c1f0bc6d7ace7d
Unknown <migu27studios@gmail.com>
2017-02-13 03:34:20 -0700

    Added final code to game object

--- Else_Statements/Assets/elseStatements.cs
+++ Else_Statements/Assets/elseStatements.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class elseStatements : MonoBehaviour {
+public class IfStatements : MonoBehaviour {
+
+	public bool myBool;
+	public string myString = "Dog";
+	public int num1;
+	public int num2;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		if (myBool)
+		{
+			print(true);
+		}
+
+		if (myString == "Cat")
+		{
+			print("Cat");
+
+		}
+		if (!myBool)
+		{
+			print("false");
+		}
+		if(myString != "Cat")
+		{
+			print(myString);
+		}
+
+		if(num1 + num2 == 7)
+		{
+			print(7);
+		}
+		if(num1 + num2 != 7)
+		{
+			print(num1 + num2);
+		}
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
